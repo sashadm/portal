2afaea410fef57b499f75e089785b6ea86daed45
sasha <alexander.dmitriev1@gmail.com>
2022-11-12 14:31:11 +0300

    git fix

--- django_portal/blog/views.py
+++ django_portal/blog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import BlogEntry
 
 
 def index(request):
-    return render(request,'base.html')
+    entries = BlogEntry.objects.all()
+    return render(request, 'index.html', {'entries': entries})
+
+
+def show_post(request,id):
+    post = get_object_or_404(BlogEntry, id=id)
+    return render(request, 'show.html', {'post': post})
--- django_portal/django_portal/urls.py
+++ django_portal/django_portal/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', index),
+    path('post/<int:id>', show_post),
 ]
