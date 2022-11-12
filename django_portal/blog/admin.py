e85a2697ac80838b9d08b101a585a4ea1f1547f5
sasha <alexander.dmitriev1@gmail.com>
2022-11-12 14:29:25 +0300

    page for posts created

--- django_portal/blog/admin.py
+++ django_portal/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Rubric)
+admin.site.register(BlogEntry)
+#admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['text','created_at']
+
+admin.site.register(Comment, CommentAdmin)
--- django_portal/blog/models.py
+++ django_portal/blog/models.py
@@ -4,12 +4,29 @@ from django.db import models
 class Rubric(models.Model):
     name = models.CharField(max_length=30, null=False)
 
+    def __str__(self):
+        return self.name
+
 
 class BlogEntry(models.Model):
-    '''title,text,data,hashtag,rubrika'''
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
     title = models.CharField(max_length=50, null=False)
-    text =  models.TextField()
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, null=False)
     rubric = models.ForeignKey(Rubric,null=True,on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField(blank=False)
+    post = models.ForeignKey(BlogEntry, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+
+    def __str__(self):
+        return f'{self.created_at.strftime("%d-%m-%Y %H:%M ")}  {self.post} -> {self.text}'
 
 
