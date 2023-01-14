16d52ef754eadcf36d361e44f6b04333a7c3d5b2
sasha <alexander.dmitriev1@gmail.com>
2023-01-14 14:00:53 +0300

    summernote added

--- django_portal/blog/admin.py
+++ django_portal/blog/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
 from .models import *
 
 admin.site.register(Rubric)
-admin.site.register(BlogEntry)
+#admin.site.register(BlogEntry)
 #admin.site.register(Comment)
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['text','created_at']
 
 admin.site.register(Comment, CommentAdmin)
+
+class BlogEntryAdmin(SummernoteModelAdmin):
+    list_display = ['title','text','created_at','rubric']
+    summernote_fields = ['text']
+
+admin.site.register(BlogEntry,BlogEntryAdmin)
--- django_portal/django_portal/urls.py
+++ django_portal/django_portal/urls.py
@@ -22,7 +22,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('editor/', include('django_summernote.urls')),
     path('post/<int:id>', show_post,name='post'),
     path('', index),
-] +static('media/', document_root=settings.MEDIA_ROOT)
+] + static('media/', document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
