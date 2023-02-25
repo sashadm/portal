e41f53a6673fbeb493d1d6d67920d426df0e8d05
sasha <alexander.dmitriev1@gmail.com>
2023-02-25 13:26:17 +0300

    forum form widget changed

--- django_portal/blog/views.py
+++ django_portal/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
 from .models import BlogEntry
 from .forms import CommentForm
@@ -13,16 +13,17 @@ def index(request):
 @login_required
 def show_post(request, id):
     post = get_object_or_404(BlogEntry, id=id)
-    comments = post.comments.all()
-    paginator = Paginator(comments, 3)
+    comments = post.comments.all().order_by('-created_at')
+    paginator = Paginator(comments, 5)
     form = None
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     if request.method == 'GET':
         form = CommentForm(initial={'post': post})
+        return render(request, 'show.html', {'page_obj': page_obj, 'post': post, 'form': form})
     if request.method == 'POST':
         form = CommentForm(request.POST)
         form.post_id = id
         if form.is_valid():
             form.save()
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'show.html', {'page_obj': page_obj, 'post': post,'form': form})
+    return redirect(f'/post/{id}')
--- django_portal/forum/forms.py
+++ django_portal/forum/forms.py
@@ -1,11 +1,12 @@
 from django.forms import ModelForm
 from .models import *
+from django_summernote.widgets import SummernoteWidget
 from django import forms
 
 
 class MessageForm(ModelForm):
     theme = forms.ModelChoiceField(label='theme', queryset=ForumTheme.objects.all(), widget=forms.HiddenInput())
-
+    text = forms.CharField(widget=SummernoteWidget(attrs={'summernote': {'width': '90%'}}))
 
     class Meta:
         model = ForumMessage
